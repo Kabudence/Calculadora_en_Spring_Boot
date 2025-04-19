62a3d15f687d0ed894fba864409796f5bc39c1f8
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-04-19 10:26:08 -0500

    feat:added initial files to spring ini

---
+++ ClculadoraenSpring-Boot/src/main/java/com/example/ClculadoraenSpring/Boot/ClculadoraenSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ClculadoraenSpring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClculadoraenSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClculadoraenSpringBootApplication.class, args);
+	}
+
+}
